0fd16559bbbe3e48601f978d3165b8d13df4f275
SOVEREIGN <isimemeni@gmail.com>
2021-01-14 22:09:21 -0500

    fixed video on movile

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className="bg-video">
-                    <video className="bg-video__content" autoPlay muted loop>
+                    <video className="bg-video__content" autoPlay muted playsInLine loop>
                         <source src={vid1} type="video/mp4" />
                         <source src={vid1} type="video/webm" />
                         Your browser is not supported!
@@ -16,12 +16,12 @@ const Header = () => {
             <div className="introduction">
                 <Row>
                   <Col md={5}>  
-                <h1 className="introduction__header" data-aos="fade-right" data-aos-duration="1000">Hello.</h1>
+                <h1 className="introduction__header" data-aos="fade-right" data-aos-duration="3000">hello.</h1>
                 </Col>
                 </Row>  
                   <Row>
                   <Col md={5}>
-                <p className="introduction__text" data-aos="fade-right" data-aos-duration="1000">"I am Isimemen Inegbedion, a Full Stack Web Developer based in Atlanta, GA. My goal is to provide clean and exciting user experiences and precise website and application functionality!
+                <p className="introduction__text" data-aos="fade-right" data-aos-duration="3000">"I am Isimemen Inegbedion, a Full Stack Web Developer based in Atlanta, GA. My goal is to provide clean and exciting user experiences and precise website and application functionality!
                 I am fluent in using HTML5, CSS, Javascript, jQuery, React, Express, Node, MySQL and MongoDB."</p>
                 </Col>
                 </Row>
